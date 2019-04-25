04d6960109b3c42149fd0ff3fc77e908044ef5e3
Lucas Romero <lucasromero65@yahoo.com>
2019-04-25 14:57:34 -0700

    Updates

--- precache-manifest.fc2e68943022bf0e407eb28994c5cfdc.js
+++ precache-manifest.fc2e68943022bf0e407eb28994c5cfdc.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
   },
   {
-    "revision": "597af27c69312e330a75",
-    "url": "/portfolio/static/js/main.597af27c.chunk.js"
+    "revision": "3239a94a1f24c84527e2",
+    "url": "/portfolio/static/js/main.3239a94a.chunk.js"
   },
   {
     "revision": "02768d6ca95453c439de",
     "url": "/portfolio/static/js/2.02768d6c.chunk.js"
   },
   {
-    "revision": "597af27c69312e330a75",
+    "revision": "3239a94a1f24c84527e2",
     "url": "/portfolio/static/css/main.5ca5a118.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/css/2.cb20852f.chunk.css"
   },
   {
-    "revision": "2ef94ecc78d0dfe265f135372c582e93",
+    "revision": "9aaa09797a844d348e426490ebd70536",
     "url": "/portfolio/index.html"
   }
 ];
